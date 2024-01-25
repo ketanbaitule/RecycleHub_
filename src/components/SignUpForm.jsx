4d870140843b5dd8ec052ab0c21f9f054720c572
Gaurav Amol Bijwe <120776034+gbijwe@users.noreply.github.com>
2024-01-26 00:36:57 +0530

    Add SignUpForm.jsx (#3)

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,10 +1,20 @@
 import { FormControl, FormLabel, Stack, Input, InputGroup, InputLeftElement, Icon, FormHelperText } from "@chakra-ui/react";
-import { BsKeyFill } from "react-icons/bs";
+import { BsKeyFill, BsPersonFill } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 
 export default function SignUpForm() {
     return (
         <FormControl>
+            <FormLabel>Name</FormLabel>
+            <Stack spacing={4}>
+                <InputGroup>
+                    <InputLeftElement pointerEvents="none" >
+                        <Icon as={BsPersonFill} />
+                    </InputLeftElement>
+                    <Input type="name" placeholder={"Ketan Baitule"} />
+                </InputGroup>
+            </Stack>
+            <br />
             <FormLabel>Email</FormLabel>
             <Stack spacing={4}>
                 <InputGroup>
@@ -25,6 +35,17 @@ export default function SignUpForm() {
                         <Icon as={BsKeyFill} />
                     </InputLeftElement>
                     <Input type="text" placeholder="Password" />
+                </InputGroup>
+            </Stack>
+            <br />
+            <FormLabel>Confirm Password</FormLabel>
+            <Stack spacing={4}>
+                <InputGroup>
+                    <InputLeftElement pointerEvents="none" >
+                        <Icon as={BsKeyFill} />
+                    </InputLeftElement>
+                    <Input type="text" placeholder="Confirm password" />
+                    <br />
                 </InputGroup>
             </Stack>
         </FormControl>
