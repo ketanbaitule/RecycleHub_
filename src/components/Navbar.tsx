5ad72402533c95ce115c7968792f2d3a08c9a7ea
Gaurav Amol Bijwe <gabijwe@gcoen.ac.in>
2024-01-25 22:09:32 +0530

    added login form

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,32 @@
-import { Box, Button, ButtonGroup, Flex, Heading, Link, Spacer } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Flex, Heading, Link, Spacer, useDisclosure } from "@chakra-ui/react";
 import NextLink from "next/link";
+import Modal from "@/components/Modal";
+import LoginForm from "./login/LoginForm";
 
 function Navbar(){
+    const { isOpen, onOpen, onClose } = useDisclosure()
     return(
+        <>
         <Flex flexDirection={{base: "column", md: "row"}} minWidth='max-content' alignItems='center' gap='2' padding={[2, 3]} className="border-b border-slate-200">
             <Box p='2'>
-                <Heading size='md'>RecycleHub</Heading>
+                <NextLink href='/'>
+                    <Heading size='md'>RecycleHub</Heading>
+                </NextLink>
             </Box>
             <Spacer display={{base: "none", md: "block"}} />
             <ButtonGroup gap='2'>
-                <NextLink href={"/signup"}>
-                    <Button colorScheme='teal'>Sign Up</Button>
-                </NextLink>
-                <NextLink href={"/login"}>
-                    <Button colorScheme='teal'>Log in</Button>
-                </NextLink>
+                <Button colorScheme='teal'>Sign Up</Button>
+                <Button colorScheme='teal' onClick={onOpen}>Log in</Button>
             </ButtonGroup>
             </Flex>
+            <Modal heading={'Login'} isOpen={isOpen} onClose={onClose} button={
+                <Button colorScheme='teal'>Login</Button>
+            }>
+                <LoginForm />
+            </Modal>
+        </>
+        
+        
     )
 }
 
