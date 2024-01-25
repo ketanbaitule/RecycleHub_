dc201fadcc2b093320545798ccc69e90dbb78f72
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-01-26 04:35:43 +0530

    Update SignUpForm.jsx

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -25,51 +25,6 @@ export default function SignUpForm() {
 
 
     return (
-        <FormControl>
-            <FormLabel>Name</FormLabel>
-            <Stack spacing={4}>
-                <InputGroup>
-                    <InputLeftElement pointerEvents="none" >
-                        <Icon as={BsPersonFill} />
-                    </InputLeftElement>
-                    <Input type="name" placeholder={"Ketan Baitule"} />
-                </InputGroup>
-            </Stack>
-            <br />
-            <FormLabel>Email</FormLabel>
-            <Stack spacing={4}>
-                <InputGroup>
-                    <InputLeftElement pointerEvents="none" >
-                        <Icon as={MdEmail} />
-                    </InputLeftElement>
-                    <Input type="email" placeholder={"whoami@xyz.com"} />
-                </InputGroup>
-            </Stack>
-            <FormHelperText>We will never share your email.</FormHelperText>
-
-            <br />
-                
-            <FormLabel>Password</FormLabel>
-            <Stack spacing={4}>
-                <InputGroup>
-                    <InputLeftElement pointerEvents="none" >
-                        <Icon as={BsKeyFill} />
-                    </InputLeftElement>
-                    <Input type="text" placeholder="Password" />
-                </InputGroup>
-            </Stack>
-            <br />
-            <FormLabel>Confirm Password</FormLabel>
-            <Stack spacing={4}>
-                <InputGroup>
-                    <InputLeftElement pointerEvents="none" >
-                        <Icon as={BsKeyFill} />
-                    </InputLeftElement>
-                    <Input type="text" placeholder="Confirm password" />
-                    <br />
-                </InputGroup>
-            </Stack>
-        </FormControl>
         <>
             { signUpStatus ? 
                 <Alert status='success' className="mb-3">
