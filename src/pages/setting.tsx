5276b90ced6468291dfbbde1ca9a8e459b77b0d8
Mayank Ashok Gongal <mayankgongal555@gmail.com>
2024-01-26 01:09:46 +0530

    profile and setting

---
+++ src/pages/setting.tsx
@@ -0,0 +1,30 @@
+import { Box, Button, ChakraProvider, FormControl, FormLabel, Input, VStack } from '@chakra-ui/react';
+import React from 'react';
+
+export default function SettingsPage() {
+  return (
+    <ChakraProvider>
+      <VStack spacing={4} align="start">
+      <FormControl id="profileImage">
+        <FormLabel>Profile Image</FormLabel>
+        <Input type="file" name="profileImage" />
+      </FormControl>
+      <FormControl id="name">
+        <FormLabel>Name</FormLabel>
+        <Input type="text" name="name" />
+      </FormControl>
+      <FormControl id="email">
+        <FormLabel>Email</FormLabel>
+        <Input type="email" name="email" />
+      </FormControl>
+      <FormControl id="location">
+        <FormLabel>Location</FormLabel>
+        <Input type="text" name="location" />
+      </FormControl>
+      <Button colorScheme="teal" variant="outline">
+        Save Changes
+      </Button>
+    </VStack>
+    </ChakraProvider>
+  );
+}
